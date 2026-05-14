590250925004ecb06790bdf3f2a9e531e96e1058
JAYOTIRA ADITIYA <j.aditiya01@gmail.com>
2026-05-14 14:43:39 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./styles/global.css"; // ✅ only once
+import "./styles/global.css";
 
 const root = document.getElementById("root");
 
